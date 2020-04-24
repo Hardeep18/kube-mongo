75e90053fdabb5f1645a6ed0150a4204b2122744
Hardeep Singh <hardeep.singh18@outlook.com>
2020-04-24 22:39:47 +0530

    updated mongodb deployment files

--- generate-secret.sh
+++ generate-secret.sh
@@ -1,3 +1,3 @@
 /usr/bin/openssl rand -base64 741 > mongosecret
-kubectl create secret generic mongosecret --from-file=mongosecret
-rm mongosecret
+kubectl create secret generic mongosecret --from-file=mongosecret -n realplay 
+#rm mongosecret
